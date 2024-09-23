3e4f7878e73dd00dd0b59b20785e00ab5bab38da
Артем Иванов <myotraykt@yandex.ru>
2024-09-23 14:32:57 +0300

    UPD 5 progr logic

--- src/games/game-progression.js
+++ src/games/game-progression.js
@@ -15,7 +15,7 @@ const playGame = () => {
     const step = getRandomNumber(1, 5);
     const start = getRandomNumber(1, 20)
     const progression = getProgression(progLength, step, start);
-    const hiddenIndex = getRandomNumber(0, progLength);
+    const hiddenIndex = getRandomNumber(0, progLength - 1);
     const correctAnswer = progression[hiddenIndex].toString();
     progression[hiddenIndex] = '..';
     return { question: progression.join(' '), correctAnswer };
