e1bce24e3bfd17c184b2bcf866673deac469034d
Артем Иванов <myotraykt@yandex.ru>
2024-09-23 14:26:53 +0300

    UPD 4 prime logic

--- src/games/game-prime.js
+++ src/games/game-prime.js
@@ -5,7 +5,7 @@ const instruction = `Answer "yes" if given number is prime. Otherwise answer "no
 const isPrime = (number) => {
     if (number < 2) return false;
     for (let i = 2; i < Math.sqrt(number); i += 1) {
-        if (number % 1 === 0) return false;
+        if (number % i === 0) return false;
     }
     return true;
 };
